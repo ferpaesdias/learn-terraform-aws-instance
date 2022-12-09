f65a9026870e6a53badbf875bee011c56fcf25df
ferpaesdias <ferpaesdias@gmail.com>
2022-12-08 21:37:04 -0300

    commit

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-east-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0283a57753b18025b"
+  instance_type = "t2.micro"
+  key_name = "IAC-Alura"
+  tags = {
+    Name = "Primeira Instancia"
+  }
+}
